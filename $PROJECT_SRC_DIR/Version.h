5e09d7c5b1043723299ce2b91db55b3b4f18abd3
shortbloke <martin@rowannet.co.uk>
2025-10-15 20:34:14 +0100

    Updated version.

--- $PROJECT_SRC_DIR/Version.h
+++ $PROJECT_SRC_DIR/Version.h
@@ -1,9 +1,9 @@
 #pragma once
 
 namespace AppVersion {
-constexpr const char kVersion[] = "1.0.13";
+constexpr const char kVersion[] = "1.0.14";
 constexpr const char kBuildDate[] = "2025-10-15";
-constexpr const char kVersionWithDate[] = "1.0.13 (2025-10-15)";
-constexpr const char kGitHash[] = "d70211c";
-constexpr const bool kDirty = true;
+constexpr const char kVersionWithDate[] = "1.0.14 (2025-10-15)";
+constexpr const char kGitHash[] = "7425573";
+constexpr const bool kDirty = false;
 }
